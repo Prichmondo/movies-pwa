3cff200811ec9ddecbb21fc991b0c9f31aaeab2d
DING\Riccardo.Volpato <riccardo.volpato@ding.com>
2020-10-18 23:10:05 +0100

    refactoring getMovie api

--- whatmovie.api/movies/database/movies.js
+++ whatmovie.api/movies/database/movies.js
@@ -15,7 +15,7 @@ module.exports.getPopularMovies = async function(userId, currentPage, itemsPerPa
           WHEN wl.movie_id IS NULL THEN false
           ELSE true
           END AS watchlist
-          ,(COUNT(r.rating) / (COUNT(r.rating) + 30) * AVG(r.rating)) + (30 / (30 + COUNT(r.rating)) * 3.6) score
+        ,(COUNT(r.rating) / (COUNT(r.rating) + 30) * AVG(r.rating)) + (30 / (30 + COUNT(r.rating)) * 3.6) score
       FROM movies AS m
       JOIN ratings AS r 
         ON r.movie_id = m.id
@@ -41,6 +41,40 @@ module.exports.getPopularMovies = async function(userId, currentPage, itemsPerPa
       .then(results => {
         const response = mapMovieResponse(results, currentPage, itemsPerPage);
         resolve(response);
+      })
+      .catch(reject);
+
+  });  
+};
+
+module.exports.getMovie = async function(userId, movieId){
+
+  return new Promise((resolve, reject) => {
+
+    const query = `
+      SELECT mo.*, AVG(ra.rating) as avgRating, ura.rating as userRating,
+      CASE 
+        WHEN wl.movie_id IS NULL THEN false
+        ELSE true
+      END AS watchlist
+      FROM movies AS mo
+      JOIN ratings AS ra ON ra.movie_id = mo.id
+      LEFT JOIN ratings AS ura ON ura.user_id = :userId AND ura.movie_id = mo.id
+      LEFT JOIN wishlist AS wl ON wl.user_id = :userId AND wl.movie_id = mo.id
+      WHERE mo.id = :movieId
+      GROUP BY mo.id
+      ORDER BY mo.title
+    `;
+
+  const params = {
+    userId: userId,
+    movieId: movieId
+  }
+
+    dbClient.query(query, params, true)
+      .then(results => {
+        const movie = results[0];
+        resolve(movie);
       })
       .catch(reject);
 
--- whatmovie.api/movies/getMovie.js
+++ whatmovie.api/movies/getMovie.js
@@ -1,7 +1,6 @@
 const utils = require('./utils');
 const ApiResponse = require('./domain/apiResponse');
-const createConnection = require('./database/createConnection');
-const mysql = require('mysql2');
+const movies = require('./database/movies');
 
 module.exports = (event, context, callback) => {
 
@@ -15,49 +14,9 @@ module.exports = (event, context, callback) => {
   if(!movieId) {
     callback(null, new ApiResponse(401, "Unknown movie"));
   }
-  
-  const query = `
-    SELECT mo.*, AVG(ra.rating) as avgRating, ura.rating as userRating,
-    CASE 
-      WHEN wl.movie_id IS NULL THEN false
-      ELSE true
-    END AS watchlist
-    FROM movies AS mo
-    JOIN ratings AS ra ON ra.movie_id = mo.id
-    LEFT JOIN ratings AS ura ON ura.user_id = :userId AND ura.movie_id = mo.id
-    LEFT JOIN wishlist AS wl ON wl.user_id = :userId AND wl.movie_id = mo.id
-    WHERE mo.id = :movieId
-    GROUP BY mo.id
-    ORDER BY mo.title
-  `;
 
-  const params = {
-    userId: userId,
-    movieId: movieId
-  }
-
-  createConnection()
-    .then(function (connection) {
-      connection.query(query, params,
-        function (error, results) {
-          if (error) {
-            connection.destroy();
-            callback(null, new ApiResponse(500, JSON.stringify(error)));
-          } else {
-
-            if(results.length === 0) {
-              callback(null, new ApiResponse(404, JSON.stringify(null)));
-            } else {
-              const movie = results[0];
-              connection.end(function (err) {
-                callback(null, new ApiResponse(200, JSON.stringify(movie)));
-              });
-            }            
-          }
-      });
-    })
-    .catch(function(error){
-      callback(null, new ApiResponse(500, JSON.stringify(error)));
-    });
+  movies.getMovie(userId, movieId)
+    .then(response => callback(null, new ApiResponse(200, JSON.stringify(response))))
+    .catch(error => callback(null, new ApiResponse(500, JSON.stringify(error))));
 
 };
